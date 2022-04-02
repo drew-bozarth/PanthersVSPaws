fe9830e2fa2c2f03d308d7372c61381e48e6a6df
Drew-bozarth <dbozarth@chapman.edu>
2022-04-01 23:50:55 -0700

    creating a repository for this project that was stored locally

---
+++ Assets/Scripts/MarkerType.cs
@@ -0,0 +1,7 @@
+﻿public enum MarkerType
+{
+    None,   // 0
+    Paw,    // 1
+    Panther,    // 2
+    Tie     // 3
+}
---
+++ Assets/Scripts/OpponentType.cs
@@ -0,0 +1,6 @@
+﻿public enum OpponentType
+{
+    Human,
+    EasyComputer,
+    HardComputer
+}
